eba8b01ef36ce26c705baf5a83d140ab6f576c4d
tamino-martinius <dev@zaku.eu>
2017-11-28 09:43:05 +0100

    Show markers of other swatches

--- src/components/new_swatch.vue
+++ src/components/new_swatch.vue
@@ -7,6 +7,28 @@
       background: swatch,
     }"
   >
+    <svg
+      viewBox="0 0 16 16"
+      v-for="(position, index) in positions"
+      :key="position.toString()"
+      :class="[
+        $style.marker,
+        {
+          [$style.activeMarker]: position.isActive
+        },
+      ]"
+      :style="{
+        left: `${position.x * 100}%`,
+        top: `${position.y * 100}%`,
+      }"
+    >
+      <circle
+        :style="{ fill: position.color }"
+        cx="8"
+        cy="8"
+        r="8"
+      ></circle>
+    </svg>
   </div>
 </template>
 
@@ -16,6 +38,7 @@
   export default {
     props: [
       'swatch',
+      'positions',
     ],
     methods: {
       handleClickEvent() {
@@ -37,7 +60,21 @@
 
 <style lang="stylus" module>
   .root {
+    position: relative;
     width: 100%;
     height: 100%;
+    cursor: crosshair;
+  }
+
+  .marker {
+    pointer-events: none;
+    position: absolute;
+    width: 8px;
+    height: 8px;
+    transform: translate(-50%, -50%);
+  }
+
+  .active-marker {
+    display: none;
   }
 </style>
