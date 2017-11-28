36b66cb2653e81e77b80e624efdec8fc77dd3155
tamino-martinius <dev@zaku.eu>
2017-11-28 09:42:51 +0100

    Add first navigation items

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -1,11 +1,43 @@
 <template>
   <div :class="$style.root">
-    Navigation
+    <ul>
+      <li
+        @click="handleSchemeClick('free')"
+      >
+        free
+      </li>
+      <li
+        :key="scheme"
+        v-for="scheme in schemes"
+        @click="handleSchemeClick(scheme)"
+      >
+        {{scheme}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
+  import { SchemeName } from '../models/scheme';
+
   export default {
+    data: () => ({
+      schemes: <SchemeName[]>[
+        'monochrome',
+        'monochromeDark',
+        'monochromeLight',
+        'analogic',
+        'complement',
+        'analogicComplement',
+        'triad',
+        'quad',
+      ]
+    }),
+    methods: {
+      handleSchemeClick(name: string) {
+        this.$emit('changeScheme', name);
+      }
+    },
   };
 </script>
 
