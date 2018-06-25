de2f973f4b2b1f11311c3aa44f9f9c1a0c415656
Tamino Martinius <dev@zaku.eu>
2018-06-25 11:32:28 +0200

    Add enum for icon types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,22 @@
 export interface Dict<T> {
   [key: string]: T;
 }
+
+export enum IconType {
+  CROSS = 'cross',
+  DOWNLOAD = 'download',
+  FACEBOOK = 'facebook',
+  GEAR = 'gear',
+  HELP = 'help',
+  LEFT = 'left',
+  LEGAL = 'legal',
+  LOGO = 'logo',
+  MINUS = 'minus',
+  NEXTCODE = 'nextcode',
+  PLUS = 'plus',
+  RIGHT = 'right',
+  SCHEME = 'scheme',
+  TOGGLES = 'toggles',
+  TRASH = 'trash',
+  TWITTER = 'twitter',
+}
