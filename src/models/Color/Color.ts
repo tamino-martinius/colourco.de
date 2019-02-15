ef290b0a4cac2cbd29f4d8fb44a89cb7335ae4ad
Tamino Martinius <dev@zaku.eu>
2019-02-15 11:11:21 +0100

    Add Color class

---
+++ src/models/Color/Color.ts
@@ -0,0 +1,31 @@
+import ColorFormat from './ColorFormat';
+
+export abstract class Color extends Array<number> {
+  public static EPSILON = 0.01;
+  public abstract bounds: number[];
+
+  constructor(...items: number[]) {
+    super(...items);
+    this.clamp();
+  }
+
+  public norm(): number[] {
+    return this.map((num, index) => num / this.bounds[index]);
+  }
+
+  public min(): number {
+    return Math.min(...this);
+  }
+
+  public max(): number {
+    return Math.max(...this);
+  }
+
+  private clamp(): void {
+    for (let index = 0; index < this.length; index += 1) {
+      this[index] = Math.max(0, Math.min(this.bounds[index]), this[index]);
+    }
+  }
+}
+
+export default Color;
