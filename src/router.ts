6dd3dab6606b974eb0fdfeb1c093493adf850661
Tamino Martinius <dev@zaku.eu>
2019-02-18 06:05:07 +0100

    Add route to freebuild

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import FreeBuild from './views/FreeBuild.vue';
 
 Vue.use(Router);
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '/free-build',
+      name: 'free-build',
+      component: FreeBuild,
     },
     {
       path: '/about',
