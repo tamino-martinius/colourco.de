324c20b1194c963a74c868f8295018f90be0701c
Tamino Martinius <dev@zaku.eu>
2019-02-18 07:34:08 +0100

    Change routes to schemes

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
-import FreeBuild from './views/FreeBuild.vue';
+import { routes } from './views/Schemes';
 
 Vue.use(Router);
 
@@ -9,23 +8,14 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/free-build',
-      name: 'free-build',
-      component: FreeBuild,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-    },
+    ...routes,
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+    // },
   ],
 });
