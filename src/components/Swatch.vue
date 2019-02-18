72f2423be9c99454af25c1bc92feb2dbc362f578
Tamino Martinius <dev@zaku.eu>
2019-02-18 07:33:15 +0100

    Add display and text color

--- src/components/Swatch.vue
+++ src/components/Swatch.vue
@@ -1,20 +1,36 @@
 <template>
-  <div class="swatch" :style="{'--color': color.toString()}">{{color}}</div>
+  <div
+    class="swatch"
+    :style="{'--background': color, '--content': displayColor, '--color': foreColor}"
+  ></div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
-import { RgbColor } from '@/models/Color';
+import { FgcColor, HexColor, RgbColor } from '@/models/Color';
 
 @Component
-export default class HelloWorld extends Vue {
+export default class Swatch extends Vue {
   @Prop() private color!: RgbColor;
+
+  private get displayColor() {
+    return `"${HexColor.fromRgb(this.color)}"`;
+  }
+
+  private get foreColor() {
+    return FgcColor.fromRgb(this.color);
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .swatch {
-  background: var(--color);
+  background-color: var(--background);
+  color: var(--color);
+}
+
+.swatch::after {
+  content: var(--content);
 }
 </style>
