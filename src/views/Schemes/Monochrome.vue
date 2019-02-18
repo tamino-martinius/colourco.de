7e0219586cea016ba555bd2743676569ed0044b4
Tamino Martinius <dev@zaku.eu>
2019-02-18 07:34:28 +0100

    Add momochrome scheme

---
+++ src/views/Schemes/Monochrome.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="free-build">
+    <Swatch :color="color"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Swatch from '@/components/Swatch.vue';
+import { RgbColor } from '@/models/Color';
+
+@Component({
+  components: {
+    Swatch,
+  },
+})
+export default class Monochrome extends Vue {
+  public color: RgbColor = new RgbColor(255, 255, 0);
+}
+</script>
