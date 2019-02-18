36cbd422ab8e14cf2ce45190f8a1eab5d645ceea
Tamino Martinius <dev@zaku.eu>
2019-02-18 06:05:33 +0100

    Add swatch to show color

---
+++ src/components/Swatch.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="swatch" :style="{'--color': color.toString()}">{{color}}</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { RgbColor } from '@/models/Color';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private color!: RgbColor;
+
+  mounted() {
+    console.log(this);
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.swatch {
+  background: var(--color);
+}
+</style>
