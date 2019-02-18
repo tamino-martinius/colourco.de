c9562c1d21ff7d6ec6daecdafe7c22cd3dbd3674
Tamino Martinius <dev@zaku.eu>
2019-02-18 07:33:56 +0100

    Add fromRgb to hex

--- src/models/Color/HexColor.ts
+++ src/models/Color/HexColor.ts
@@ -1,6 +1,10 @@
 import RgbColor from './RgbColor';
 
 export class HexColor extends RgbColor {
+  public static fromRgb(rgb: RgbColor): HexColor {
+    return new HexColor(...rgb.values);
+  }
+
   public toString() {
     const values = this.map((num) => `${num.toString(16).padStart(2, '0')}`);
     return `#${values.join('')}`;
