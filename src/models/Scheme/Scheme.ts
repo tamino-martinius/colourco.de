c1ef17f91513711548bb2f23041d6de3c184dd09
Tamino Martinius <dev@zaku.eu>
2019-05-06 07:27:23 +0200

    Change scheme to create colors with generate

--- src/models/Scheme/Scheme.ts
+++ src/models/Scheme/Scheme.ts
@@ -9,31 +9,35 @@ export interface Mapping {
 
 export class Scheme {
   public mappings: Mapping[] = [];
-  public colors: RgbColor[];
 
-  constructor(hsl: HslColor, count: number) {
+  generate(centerHsl: HslColor, count: number) {
     const colors: RgbColor[] = [];
     const counts = this.getCounts(count);
-    const hslNorm = hsl.norm();
+    const hslNorm = centerHsl.norm();
     const range = 0.5;
     const halfRange = range / 2;
     const rangeStep = range / count;
     let seeds = hslNorm.map((num) => Math.max(0, Math.min(1, num + halfRange) - range) - num);
+
     this.mappings.forEach((mapping, mappingIndex) => {
       for (let colorIndex = 0; colorIndex < counts[mappingIndex]; colorIndex += 1) {
         const [h, s, l] = hslNorm.map((num, index) => mapping.origins[index](num, seeds[index]));
+        const hsl = new HslColor(h * 360, s * 100, l * 100);
+        const rgb = hsl.toRgb();
+
+        colors.push(rgb);
         seeds = seeds.map((seed) => (seed += rangeStep));
-        colors.push(new HslColor(h * 360, s * 100, l * 100).toRgb());
       }
     });
 
-    this.colors = colors;
+    return colors;
   }
 
   private getCounts(count: number): number[] {
     const ratios: number[] = this.mappings.map((mapping) => mapping.ratio);
-    const counts: number[] = this.mappings.map(() => 0);
+    const counts: number[] = ratios.map((r) => 0);
     const weight = 1 / count;
+
     for (let i = 0; i < count; i++) {
       let maxRatio = -1;
       let maxRatioIndex = -1;
